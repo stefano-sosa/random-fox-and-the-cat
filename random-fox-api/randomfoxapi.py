a69a99faa90972672bbc2b5f2300da88a7f0dd51
Stefano Sosa <stefano.sosac@gmail.com>
2022-10-19 15:15:27 -0500

    documentación de la clase, de los métodos, y nuevo método

--- random-fox-api/randomfoxapi.py
+++ random-fox-api/randomfoxapi.py
@@ -25,20 +25,61 @@ except ModuleNotFoundError:
     message('requests')
     
 class randomfoxAPI:
+    """
+    Class used to request a random image of an fox
+
+    Attributes
+    ----------
+    img : Image
+        Image requested
+    imgsize : tuple
+        the dimensions of the image
+
+    Methods
+    -------
+    req_image()
+        Request the image
+        
+    resize_image(size)
+        Replaces the existing image with a new one with the dimensions indicated in 'size'
+        
+    save_image(name, path)
+        Saves the existing
+    """
     
     def __init__(self):
+        """
+        Parameters:
+        ----------
+        No parameters
+        
+        Description:
+        -----------
+        Inits the class
+        """
         self.__imgurl = None
         self.__imgname = None
         self.img = None
         self.imgsize = None
+        self.__orgsize = None
         self.__baseurl = 'https://randomfox.ca/floof/'
     
     def req_image(self):
+        """
+        Parameters
+        ----------
+        No parameters
+        
+        Description:
+        -----------
+        Performs a GET HTTP request to the endpoint of the API, and stores the image in the img attribute
+        """
         req = requests.get(self.__baseurl)
         load = json.loads(req.content)
         self.__imgurl = load['image']
         imagereq = requests.get(load['image'])
-        self.img = Image.open(BytesIO(imagereq.content))
+        self.__original = Image.open(BytesIO(imagereq.content)) 
+        self.img = self.__original
         self.imgsize = self.img.size
 
     def __arg_int(self, myint):
@@ -68,14 +109,47 @@ class randomfoxAPI:
         raise ValueError(f'{arg} is {type(arg)}. The argument must an integer, a list, or a tuple.')
         
     def resize_image(self, size=()):
+        """
+        Parameters
+        ----------
+        size: int, tuple, list
+        
+        Description:
+        -----------
+        Resize the existing image with the size specified in 'size', and then replaces it.
+        """
         try:
             isize = self.__switch_arg(size)
             self.img = self.img.resize(isize)
         except TypeError:
             a, b = size
             print(f'{a} is {type(a)}, {b} is {type(b)}, and both must be integers')
+            
+    def restore_image(self):
+        """
+        Parameters
+        ----------
+        No parameters
         
-    def save_image(self, *, path='', name=''):
+        Description:
+        -----------
+        Replaces the existing image with the original one.
+        """
+        self.img = self.__original
+        
+    def save_image(self, *, name='', path=''):
+        """
+        Parameters
+        ----------
+        path: str
+        name: str
+        
+        Description:
+        -----------
+        Saves the image requested  in the path indicated in path, with name indicated in name.
+        If no name is indicated, the class will use the name indicated in the response of the API.
+        If no path is indicated the class will save the image in /tmp
+        """
         img = self.img.convert('RGB')
 
         if name:
